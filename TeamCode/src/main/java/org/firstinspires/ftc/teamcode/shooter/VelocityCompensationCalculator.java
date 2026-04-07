17f0e3c8c9e7b796543e506f8619d7896d74882f
billwillknot <bill.will.knot@gmail.com>
2026-04-07 19:09:28 -0400

    fudged  comp vals

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/shooter/VelocityCompensationCalculator.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/shooter/VelocityCompensationCalculator.java
@@ -17,7 +17,7 @@ import java.util.stream.IntStream;
 
 @Config
 public class VelocityCompensationCalculator {
-    public static double kAutoRad = 7;
+    public static double kAutoRad = 5;
     public static double kAutoTan = 5;
 
     public static boolean useAutoLimit = true;
@@ -55,10 +55,10 @@ public class VelocityCompensationCalculator {
                     Constants.BLUE_GOAL_POSE.mirror()))
             .toArray();
 
-    public static double[] flywheelSpeedValues = {918, 925, 1068, 1125, 1161, 1209, 1320, 1317, 1426};
+    public static double[] flywheelSpeedValues = {918, 925, 1068, 1125, 1161, 1220, 1330, 1337, 1446};
 
     public static double[] adjustedFlywheelSpeedValues = IntStream.range(0, flywheelSpeedValues.length)
-            .mapToDouble(i -> flywheelSpeedValues[i] + 0) //BILL WILLIAMS
+            .mapToDouble(i -> flywheelSpeedValues[i] + 5) //BILL WILLIAMS
             .toArray();
 
     public static double[] hoodServoValues      = {0.04, 0.05, 0.06, 0.06, 0.07, 0.08, 0.08, 0.08, 0.09};
