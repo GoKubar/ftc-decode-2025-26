5640d4db0c3acd9433adf4d3e71ba10ac1e37dfd
Kabir <kabirgoyal@icloud.com>
2026-04-08 14:32:28 -0400

    reverted wtf vedic did to the auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Auto.java
@@ -1,7 +1,6 @@
 package org.firstinspires.ftc.teamcode.opmodes;
 
 import static com.pedropathing.ivy.Scheduler.schedule;
-import static com.pedropathing.ivy.commands.Commands.infinite;
 import static com.pedropathing.ivy.commands.Commands.instant;
 import static com.pedropathing.ivy.commands.Commands.waitMs;
 import static com.pedropathing.ivy.commands.Commands.waitUntil;
@@ -30,16 +29,9 @@ import org.firstinspires.ftc.teamcode.robot.Robot;
 import org.firstinspires.ftc.teamcode.robot.States;
 import org.firstinspires.ftc.teamcode.shooter.VelocityCompensationCalculator;
 import org.firstinspires.ftc.teamcode.util.telemetry.FastTelemetry;
-import org.firstinspires.ftc.teamcode.vision.Pipelines;
-import org.firstinspires.ftc.teamcode.vision.VisionPipeline;
-import org.firstinspires.ftc.teamcode.vision.pipelines.RelocalizationPipeline;
 
 public abstract class Auto extends LinearOpMode {
     Robot robot;
-
-    double ARTIFICIAL_DRIFT_INCHES = 24.0;
-    RelocalizationPipeline relocPipeline;
-
     //default for all poses is blue side
 
     //length = 15.39
@@ -485,21 +477,7 @@ public abstract class Auto extends LinearOpMode {
 //        Constants.robot = robot;
         robot.setPose(startPose);
         robot.setLocalizationMode(Robot.LocalizationMode.FOLLOWER);
-
-        robot.setPipeline(Pipelines.RELOCALIZATION);
-
-        VisionPipeline rawPipeline = robot.getLimelightManager() != null
-                ? robot.getLimelightManager().getCurrentPipeline()
-                : null;
-
-        if(rawPipeline instanceof RelocalizationPipeline)
-            relocPipeline = (RelocalizationPipeline) rawPipeline;
-
         robot.init();
-//        robot.updateDriveCommand = infinite(robot::updateDrive);
-//        Scheduler.schedule(robot.updateDriveCommand);
-//        robot.setState(States.NONE);
-        telemetry.update();
 
         generatePaths();
     }
@@ -508,52 +486,11 @@ public abstract class Auto extends LinearOpMode {
         initialize();
 
         waitForStart();
-
         robot.setState(States.INTAKING);
         createAutoCommands();
         while (opModeIsActive()) {
             robot.clearCaches();
-
-            Pose drifted = new Pose(
-                    robot.getPose().getX() + ARTIFICIAL_DRIFT_INCHES,
-                    robot.getPose().getY(),
-                    robot.getPose().getHeading());
-            robot.setPose(drifted);
-
-            addVisionTelemetry();
             Scheduler.execute();
         }
-    }
-
-    private void addVisionTelemetry() {
-        Pose currentPose = robot.getPose();
-
-
-        telemetry.addData("Odometry X (in)", "%.2f", currentPose.getX());
-        telemetry.addData("Odometry Y (in)", "%.2f", currentPose.getY());
-        telemetry.addData("Odometry Heading (deg)", "%.1f", Math.toDegrees(currentPose.getHeading()));
-
-        if (relocPipeline != null) {
-            telemetry.addLine("--- MT2 Vision ---");
-            telemetry.addData("Tags Visible", relocPipeline.getLastTagCount());
-            telemetry.addData("Total Corrections", relocPipeline.getCorrectionCount());
-            telemetry.addData("CORRECTED THIS LOOP", relocPipeline.correctedThisLoop() ? "YES <<" : "no");
-
-            Pose lastFix = relocPipeline.getLastCorrectedPose();
-            if (lastFix != null) {
-                telemetry.addData("Last MT2 X (in)", "%.2f", lastFix.getX());
-                telemetry.addData("Last MT2 Y (in)", "%.2f", lastFix.getY());
-                telemetry.addData("Last MT2 Heading (deg)", "%.1f", Math.toDegrees(lastFix.getHeading()));
-
-                double dx = currentPose.getX() - lastFix.getX();
-                double dy = currentPose.getY() - lastFix.getY();
-                telemetry.addData("Delta to Last MT2 (in)", "%.2f", Math.hypot(dx, dy));
-            } else {
-                telemetry.addLine("No mega 2");
-            }
-        } else {
-            telemetry.addLine("no lim");
-        }
-
     }
 }
