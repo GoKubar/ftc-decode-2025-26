f2c41c8aec96649327fe230b4420de2ef10e9457
Kabir <kabirgoyal@icloud.com>
2026-04-18 11:46:49 -0400

    auto changes for comp field

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/CompatabilityAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/CompatabilityAuto.java
@@ -11,7 +11,7 @@ public abstract class CompatabilityAuto extends Auto{
         updateShooter = robot.updateShootingSubsystems();
 //        updateTurret = robot.updateTurret();
 
-        double shootTime = 450;
+        double shootTime = 550;
 
         schedule(
                 updateShooter,
